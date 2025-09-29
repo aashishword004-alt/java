aa6f37ede4ca1034184775951fa741286a24d29c
aashishword004-alt <aashishword004@gmail.com>
2025-09-29 22:28:49 +0530

    outputmathod

---
+++ java_2ex.java
@@ -0,0 +1,20 @@
+class java_2ex {
+    public static void main(String args[]) {
+        // 1 output println
+        System.out.println("Hello word java");
+       System.out.println("Hello word java");
+       
+       // output 3
+       System.out.print("Hiyy Ashish \n Parmar");
+      
+       // 2 output print
+       System.out.print("Hello word java ");
+       System.out.print("Hello word java ");
+
+
+
+
+
+    }
+
+}

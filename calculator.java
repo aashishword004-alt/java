4418ebc5f962c57c923444c0df2ca2451ddacfcf
aashishword004-alt <aashishword004@gmail.com>
2025-09-30 13:27:29 +0530

    calculatoredone

---
+++ calculator.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main (String args[])
+    {
+       Scanner sc = new Scanner(System.in);
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+       int button = sc.nextInt();
+       int add = a+b;
+       int sub = a-b;
+       int mul = a*b;
+       double div = a/b;
+
+       switch (button) {
+        case 1: System.out.println(add);
+        break;
+        case 2 : System.out.println(sub);   
+        break;
+        case 3: System.out.println(mul);
+        break;
+        case 4: System.out.println(div);
+        break;
+        default:System.out.println("Zero");
+            break;
+       }
+
+
+
+    
+    }
+    
+}

e2874ac759a03de74a8e8df2f804858f57edb7a8
aashishword004-alt <aashishword004@gmail.com>
2025-10-01 13:00:02 +0530

    pattern

---
+++ patternt.java
@@ -0,0 +1,11 @@
+public class patternt {
+    public static void main(String args[])
+    {
+        int i = 5;
+        for(int n=0; n<=i; n++)
+        {
+            System.out.println("*");
+        }
+    }
+    
+}
---
+++ patternthree.java
@@ -0,0 +1,22 @@
+public class patternthree {
+    public static void main(String   args[])
+    {
+        // nested loop
+         int n = 4;
+         int m = 5;
+       
+           // out loop
+           for(int i=1; i<=n; i++)
+           {
+            // inner loop
+            for(int j=1; j<=m; j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+           }
+
+        }
+    }
+    
+

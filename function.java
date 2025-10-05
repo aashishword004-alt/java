8cff8ef6cedfec9e74204467278686adc9af0a81
aashishword004-alt <aashishword004@gmail.com>
2025-10-05 21:58:42 +0530

    function

---
+++ function.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class function {
+    public static void printMyname(String name){
+        System.out.println(name);
+        return ;
+      
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        printMyname(name);
+        
+    }
+    
+}
+ 
---
+++ functiontwo.java
@@ -0,0 +1,12 @@
+public class functiontwo {
+   public static void calCulator(int a , int b)
+   {
+
+   }
+   
+    public static void main(String args[])
+    {
+
+    }
+    
+}

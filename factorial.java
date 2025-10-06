aba6c459d3482fd897ed7ab01bb5f95ddd72e708
aashishword004-alt <aashishword004@gmail.com>
2025-10-06 12:42:53 +0530

    functions

---
+++ factorial.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class factorial {
+
+    public static void fact(int n){
+
+        if(n<0)
+        {
+            System.out.print("Invalid Number");
+            return;
+        }
+    // loop 
+    int factorial = 1;
+    for(int i=n; i>=1; i--)
+
+    {
+        factorial = factorial *i;
+    }
+    System.out.println(factorial);
+    return;
+    }
+
+    public static void main(String args[])
+    {
+    Scanner sc =new Scanner(System.in);
+    int n = sc.nextInt();
+    fact(n);
+    }
+    
+}
--- functiontwo.java
+++ functiontwo.java
@@ -1,12 +1,21 @@
-public class functiontwo {
-   public static void calCulator(int a , int b)
-   {
+import java.util.Scanner;
 
+public class functiontwo {
+   public static int calCulator(int a , int b)
+   {
+       int sum = a*b;
+       return sum;     
    }
    
     public static void main(String args[])
-    {
 
-    }
+    {
+     Scanner sc = new Scanner(System.in);
+     int a = sc.nextInt();
+     int b = sc.nextInt();
+
+     int sum = calCulator(a,b);
+     System.out.print("Answer is " + sum);
+    } 
     
 }

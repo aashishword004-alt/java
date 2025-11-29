62802603687d27e94d4e9ef62a871a8b5f0afb7c
aashishword004-alt <aashishword004@gmail.com>
2025-11-29 18:26:16 +0530

    array in java

--- array_1.java
+++ array_1.java
@@ -1,10 +1,11 @@
 public class array_1 {
     // array Syantex
 
+    // for print array 
     public static void main(String args[]) {
         // int[] marks = new int[3];
         // int marks[] = new int[3];
-        int marks [] = {97,98,91};
+        int marks [] = {97,98,91,98};
         
         // marks[0] = 98;
         // marks[1] = 90;
@@ -13,7 +14,7 @@ public class array_1 {
         // System.out.println(marks[1]);
         // System.out.println(marks[2]);
 
-        for(int i=0; i<3; i++)
+        for(int i=0; i<4; i++)
         {
             System.out.println(marks[i]);
         }
